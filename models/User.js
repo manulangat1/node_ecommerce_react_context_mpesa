6e3bedcfa0348f3044211938f3d905e168b4803f
manulangat <emmanuelthedeveloper@gmail.com>
2020-08-08 21:04:17 +0300

    ft:adding_password_hashing_and_jwt_tokens:backend

--- models/User.js
+++ models/User.js
@@ -26,9 +26,48 @@ const UserSchema = new mongoose.Schema({
     activationString:{
         type:String,
         trim:true
-    }
+    },
+    isAdmin:{
+        type:Boolean,
+        required:[true,"Please input isAdmin"],
+        default:false
+    },
+    tokens:[{
+        token:{
+            type:String,
+            required:true
+        }
+    }]
 })
 
+UserSchema.pre('save', async function(next){
+//hash the password before saving the user 
+    const user = this 
+    if (user.isModified('password')){
+        user.password = await bcrypt.hash(user.password,8)
+    }
+})
+//generateAuthTokenMethod 
+UserSchema.methods.generateAuthToken = async function(){
+    const user = this
+    const token = jwt.sign({_id:user._id,username:user.username},process.env.JWT_KEY)
+    user.tokens = user.tokens.concat({token})
+    await user.save()
+    return token 
+}
+
+UserSchema.statics.findByCredentials = async (email, password) => {
+    // Search for a user by email and password.
+    const user = await User.findOne({ email} )
+    if (!user) {
+        throw new Error({ error: 'Invalid login credentials' })
+    }
+    const isPasswordMatch = await bcrypt.compare(password, user.password)
+    if (!isPasswordMatch) {
+        throw new Error({ error: 'Invalid login credentials' })
+    }
+    return user
+}
 
 const User = mongoose.model('User',UserSchema)
 module.exports = User
