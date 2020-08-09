a1e0fee0f771e59462f1e2af5580e7398ab58ee5
manulangat <emmanuelthedeveloper@gmail.com>
2020-08-09 16:52:28 +0300

    ft:done_refactor:backend

--- routes/Order.js
+++ routes/Order.js
@@ -2,8 +2,10 @@ const express = require('express')
 
 const { getOrder,postOrder } = require('../controllers/Order')
 
+const auth = require('../middlewares/isAuth')
+
 const router = express.Router()
 
-router.route('/').get(getOrder).post(postOrder)
+router.route('/').get(auth,getOrder).post(auth,postOrder)
 
 module.exports = router
