861253f8eb4e045e694c8855d19e942d1733efee
manulangat <emmanuelthedeveloper@gmail.com>
2020-08-11 19:56:39 +0300

    ht

--- routes/Product.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const { getProduct , postProduct } = require('../controllers/Product')
-const auth = require('../middlewares/isAuth')
-const router = express.Router()
-
-router.route('/').get(getProduct).post(auth,postProduct)
-
-module.exports = router
